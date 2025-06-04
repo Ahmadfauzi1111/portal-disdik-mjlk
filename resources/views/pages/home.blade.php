d08d1494cb69f897a3a2870f9431cde4365201a2
Ahmadfauzi1111 <ahmadfauziridlwan20@gmail.com>
2025-06-04 13:30:35 +0700

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.lp')
+
+@section('title', 'Dinas Pendidikan Majalengka')
+
+@section('content')
+<div class="relative overflow-hidden">
+    @include('components.home.jumbotron')
+    @include('components.home.news')
+    {{-- @include('components.home.event') --}}
+    @include('components.home.service')
+</div>
+@endsection
+
+@section('meta')
+<meta name=title content="Dinas Pendidikan Majalengka">
+        <meta name="description" content="Dinas Pendidikan Majalengka">
+        <meta name="keywords" content="Dinas Pendidikan Majalengka">
+        <meta name="author" content="Dinas Pendidikan Majalengka">
+
+        <script type="application/ld+json">
+            {
+                "@context": "http://schema.org",
+                "@type": "WebSite",
+                "url": "{{ config('app.url') }}",
+            }
+        </script>
+@endsection
